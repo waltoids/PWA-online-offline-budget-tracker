23b1e3e96a183a06eec115d7abc5586c92982183
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-26 20:01:47 -0400

    fixed fetch issue with event listner, linted file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,55 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "/manifest.webmanifest",
-  ];
-  
-  
-  const CACHE_NAME = "static-cache-v1";
-  const DATA_CACHE_NAME = "data-cache-v2";
-  
-  self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener("activate", event => {
-    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-      }).then(cachesToDelete => {
-        return Promise.all(cachesToDelete.map(cacheToDelete => {
-          return caches.delete(cacheToDelete);
-        }));
-      }).then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener("fetch", event => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-          if (cachedResponse) {
-            return cachedResponse;
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/manifest.webmanifest',
+];
+
+const CACHE_NAME = 'static-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting()),
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cache) => {
+        if (cache !== CACHE_NAME && cache !== DATA_CACHE_NAME) {
+          console.log('Removing chace data', cache);
+          return caches.delete(cache);
+        }
+      }),
+    )),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => fetch(event.request)
+        .then((response) => {
+          if (response.status === 200) {
+            cache.put(event.request.url, response.clone());
           }
-  
-          return caches.open(DATA_CACHE_NAME).then(cache => {
-            return fetch(event.request).then(response => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
+
+          return response;
         })
-      );
-    }
-  });
+        .catch(() => cache.match(event.request)))
+        .catch((err) => console.log(err)),
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request)),
+  );
+});
